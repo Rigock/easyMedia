e44547096847a9ef7422fc2de6b4fcbaddb6196a
Rigock <rigock11@gmail.com>
2023-07-11 17:26:51 -0500

    Initial config index

--- back/build/index.js
+++ back/build/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var morgan_1 = __importDefault(require("morgan"));
+var cors_1 = __importDefault(require("cors"));
 var indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 var appRoutes_1 = __importDefault(require("./routes/appRoutes"));
 var Server = /** @class */ (function () {
@@ -16,6 +17,9 @@ var Server = /** @class */ (function () {
     Server.prototype.config = function () {
         this.app.set('port', process.env.PORT || 3000);
         this.app.use(morgan_1.default('dev'));
+        this.app.use(cors_1.default());
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     };
     Server.prototype.routes = function () {
         this.app.use('/', indexRoutes_1.default);
