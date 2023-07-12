18e3d9d8f56e8b2711559566808dcabbf162c58c
Rigock <rigock11@gmail.com>
2023-07-12 17:19:56 -0500

    Update styles

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from "./components/post-list/post-list.component";
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'posts',
     component: PostListComponent
+  },
+  {
+    path: 'posts/add',
+    component: PostFormComponent
   }
 ];
 
