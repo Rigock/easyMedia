3292289b90371eaf4d05295ec8b7f519af60e9bc
Rigock <rigock11@gmail.com>
2023-07-12 11:09:01 -0500

    clean code

--- database/database.sql
+++ database/database.sql
@@ -2,21 +2,23 @@ CREATE DATABASE ng_easyMedia_db;
 
 USE  ng_easyMedia_db;
 
-CREATE TABLE state(
+CREATE TABLE states(
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR(30) NOT NULL,
     message VARCHAR(300),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     author VARCHAR(100)
+    -- user_id INT(11),
+    -- CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE user(
+CREATE TABLE users(
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    password VARCHAR(100)
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
 );
 
-DESCRIBE state;
+DESCRIBE states;
 
-DESCRIBE user;
+DESCRIBE users;
