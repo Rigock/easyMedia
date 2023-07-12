592560e063bad8fe55481881584504f3b4a22f31
Rigock <rigock11@gmail.com>
2023-07-12 11:39:54 -0500

    Directory created

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    PostFormComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
