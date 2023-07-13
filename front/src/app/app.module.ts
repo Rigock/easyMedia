51d2ed79d0d5a4f526d8ac5a639f531471307a3a
Rigock <rigock11@gmail.com>
2023-07-13 00:59:09 -0500

    Page create a post done

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { PostsService } from "./services/posts.service";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [
     PostsService
